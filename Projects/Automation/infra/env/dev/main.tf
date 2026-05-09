bb74a4cd989f0ceb92e3e50865309b7c2e6fe168
RaduBRB <duku228@gmail.com>
2026-05-09 22:33:16 +0100

    Add CI/CD pipe

--- Projects/Automation/infra/env/dev/main.tf
+++ Projects/Automation/infra/env/dev/main.tf
@@ -2,12 +2,46 @@ provider "aws" {
     region = var.region
 }
 
-resource "aws_ecr_repository" "Procode_ECR_Repository" {
-    name = "procode-ecr-repository"
-    image_tag_mutability = "IMMUTABLE"
-    image_scanning_configuration {
-      scan_on_push = true
-    }
-  
+module "ecr" {
+  source = "../../modules/ecr"
+
+  repo_name = "procode-ecr-repository"
+}
+module "alb" {
+  source = "../../modules/alb"
+
+  vpc_id         = module.vpc.vpc_id
+  public_subnets = module.vpc.public_subnets
 }
 
+module "ecs" {
+  source = "../../modules/ecs"
+
+  vpc_id = module.vpc.vpc_id
+
+  subnets = module.vpc.public_subnets
+
+  alb_sg_id        = module.alb.alb_security_group_id
+  target_group_arn = module.alb.target_group_arn
+  listener_arn     = module.alb.listener_arn
+
+  image_url = "991150654740.dkr.ecr.us-east-1.amazonaws.com/procode-ecr-repository:latest"
+}
+
+module "vpc" {
+  source = "../../modules/vpc"
+
+  vpc_cidr = "10.0.0.0/16"
+
+  azs = ["us-east-1a", "us-east-1b"]
+
+  public_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ]
+
+  private_subnets = [
+    "10.0.3.0/24",
+    "10.0.4.0/24"
+  ]
+}
